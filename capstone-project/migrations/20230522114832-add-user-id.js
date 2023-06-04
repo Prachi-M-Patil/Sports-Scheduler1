d8585fa029fc17e87a49f43f24437084a6ed2766
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-06-04 23:36:56 +0530

    Create 20230522114832-add-user-id.js

---
+++ capstone-project/migrations/20230522114832-add-user-id.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("session", "userId", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("session", {
+      fields: ["userId"],
+      type: "foreign key",
+      references: {
+        table: "user",
+        field: "id",
+      },
+    });
+
+    await queryInterface.addColumn("sports", "userId", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+
+    await queryInterface.addConstraint("sports", {
+      fields: ["userId"],
+      type: "foreign key",
+      references: {
+        table: "user",
+        field: "id",
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("session", "userId");
+    await queryInterface.removeColumn("sports", "userId");
+  },
+};

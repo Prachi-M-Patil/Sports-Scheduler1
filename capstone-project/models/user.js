c697eccb3ddf5149191af47b679fadba9b5c104b
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2023-06-04 23:38:41 +0530

    Create user.js

---
+++ capstone-project/models/user.js
@@ -0,0 +1,65 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+  
+    static associate(models) {
+      // define association here
+      user.hasMany(models.session, {
+        foreignKey: "userId",
+      });
+      user.hasMany(models.sports, {
+        foreignKey: "userId",
+      });
+    }
+    static async getUser(userId) {
+      return this.findByPk(userId);
+    }
+
+    static async AddsessionIdinuser(sessionId, userId) {
+      const getUser = await this.getUser(userId);
+      //console.log(getUser)
+      getUser.sessionId.push(sessionId);
+      return this.update(
+        {
+          sessionId: getUser.sessionId,
+        },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    }
+
+    static async removeSessionId(sessionId, userId) {
+      const getUser = await this.getUser(userId);
+      getUser.sessionId.pop(sessionId);
+      return this.update(
+        {
+          sessionId: getUser.sessionId,
+        },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    }
+  }
+  user.init(
+    {
+      fname: DataTypes.STRING,
+      lname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+      sessionId: DataTypes.ARRAY(DataTypes.INTEGER),
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
+  return user;
+};
